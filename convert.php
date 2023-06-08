daef56749a30c302eb2ec29ee11f4c1a9bb47ae9
Programmer Xiaobai <85952769+158456645113@users.noreply.github.com>
2023-06-08 16:47:57 +0800

    Update convert.php

--- convert.php
+++ convert.php
@@ -1,33 +1,41 @@
 <?php
-if(isset($_FILES['file'])) {
-    $file_name = $_FILES['file']['name'];
-    $file_size = $_FILES['file']['size'];
-    $file_tmp = $_FILES['file']['tmp_name'];
-    $file_type = $_FILES['file']['type'];
-    $file_ext = strtolower(end(explode('.', $_FILES['file']['name'])));
-    
-    $extensions = array("jpeg","jpg","png");
-    
-    if(in_array($file_ext, $extensions) === false){
-        echo "文件扩展名不允许，请上传 JPEG 或 PNG 文件。";
-    }
-    
-    if($file_size > 2097152) {
-        echo "文件大小不能超过 2 MB。";
-    }
-    
-    $upload_path = "uploads/";
-    $upload_file = $upload_path . $file_name;
-    
-    if(move_uploaded_file($file_tmp, $upload_file)) {
-        echo "文件上传成功。";
-    } else {
-        echo "文件上传失败。";
-    }
+$targetDir = "uploads/";
+$targetFile = $targetDir . basename($_FILES["video"]["name"]);
+$uploadOk = 1;
+$videoFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+// 允许的视频文件类型
+$allowedTypes = array("mp4", "avi", "mov");
+
+// 检查文件类型
+if (!in_array($videoFileType, $allowedTypes)) {
+  echo "仅支持上传 MP4、AVI 或 MOV 格式的视频文件";
+  $uploadOk = 0;
+}
+
+// 检查上传错误
+if ($_FILES["video"]["error"] > 0) {
+  echo "文件上传发生错误";
+  $uploadOk = 0;
+}
+
+// 检查文件大小
+if ($_FILES["video"]["size"] > 50000000) { // 限制文件大小为 50MB
+  echo "文件大小超过限制";
+  $uploadOk = 0;
+}
+
+// 如果文件符合要求，则移动到指定目录
+if ($uploadOk == 1) {
+  if (move_uploaded_file($_FILES["video"]["tmp_name"], $targetFile)) {
+    // 进行视频转换和序列图生成的代码
+    // ...
+
+    // 返回序列图的下载链接
+    $result = "sequence_images.zip";
+    echo $result;
+  } else {
+    echo "文件上传失败";
+  }
 }
 ?>
-
-<form action="" method="POST" enctype="multipart/form-data">
-    <input type="file" name="file">
-    <input type="submit" value="上传">
-</form>
