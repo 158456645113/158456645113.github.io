93954a5f02f062922b8131852879ddd0cbafb322
Programmer Xiaobai <85952769+158456645113@users.noreply.github.com>
2023-06-08 16:29:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 function uploadVideo(event) {
     event.preventDefault();
-    
+
     var form = document.getElementById('uploadForm');
     var formData = new FormData(form);
-    
+
     var request = new XMLHttpRequest();
     request.open('POST', 'convert.php');
-    
+
     request.upload.addEventListener('progress', function(event) {
         var progress = Math.round((event.loaded / event.total) * 100);
         document.getElementById('progress').innerHTML = '上传进度: ' + progress + '%';
     });
-    
+
     request.addEventListener('load', function(event) {
         if (request.status == 200) {
             var response = JSON.parse(request.responseText);
             if (response.success) {
-                document.getElementById('result').innerHTML = '转换完成! <a href="' + response.zipUrl + '">下载序列图</a>';
+                document.getElementById('progress').innerHTML = '转换完成!';
+                document.getElementById('result').innerHTML = '点击下载：<a href="' + response.zipUrl + '">序列图 ZIP 文件</a>';
             } else {
-                document.getElementById('
+                document.getElementById('progress').innerHTML = '转换失败!';
+            }
+        }
+    });
+
+    request.send(formData);
+}
