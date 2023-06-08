9c1d7b58e7fa40a2c348c5e94c10f01c5ea87309
Programmer Xiaobai <85952769+158456645113@users.noreply.github.com>
2023-06-08 16:41:22 +0800

    Update convert.php

--- convert.php
+++ convert.php
@@ -1,32 +1,33 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $videoFile = $_FILES['videoFile'];
-
-    // 创建一个临时目录用于存放生成的图片序列
-    $tempDir = 'temp/' . uniqid();
-
-    // 创建临时目录
-    mkdir($tempDir);
-
-    // 获取视频的关键帧图片
-    $ffmpegCommand = "ffmpeg -i " . $videoFile['tmp_name'] . " -vf 'select=eq(pict_type\,I)' -vsync vfr " . $tempDir . "/%03d.jpg";
-    exec($ffmpegCommand, $output, $returnCode);
-
-    if ($returnCode === 0) {
-        // 将生成的图片序列打包为 ZIP 文件
-        $zipFile = 'sequence.zip';
-        $zipCommand = "zip -j -r " . $zipFile . " " . $tempDir;
-        exec($zipCommand);
-
-        // 删除临时目录及其内容
-        $deleteCommand = "rm -rf " . $tempDir;
-        exec($deleteCommand);
-
-        // 返回 ZIP 文件的相对路径
-        echo json_encode(['success' => true, 'zipUrl' => $zipFile]);
+if(isset($_FILES['file'])) {
+    $file_name = $_FILES['file']['name'];
+    $file_size = $_FILES['file']['size'];
+    $file_tmp = $_FILES['file']['tmp_name'];
+    $file_type = $_FILES['file']['type'];
+    $file_ext = strtolower(end(explode('.', $_FILES['file']['name'])));
+    
+    $extensions = array("jpeg","jpg","png");
+    
+    if(in_array($file_ext, $extensions) === false){
+        echo "文件扩展名不允许，请上传 JPEG 或 PNG 文件。";
+    }
+    
+    if($file_size > 2097152) {
+        echo "文件大小不能超过 2 MB。";
+    }
+    
+    $upload_path = "uploads/";
+    $upload_file = $upload_path . $file_name;
+    
+    if(move_uploaded_file($file_tmp, $upload_file)) {
+        echo "文件上传成功。";
     } else {
-        // 转换失败
-        echo json_encode(['success' => false]);
+        echo "文件上传失败。";
     }
 }
 ?>
+
+<form action="" method="POST" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <input type="submit" value="上传">
+</form>
