fcfa76c4662f3083dd4b3fa7bdf76e21ff99f3ef
Programmer Xiaobai <85952769+158456645113@users.noreply.github.com>
2023-06-08 16:25:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,24 @@
-function convertVideo() {
-  var videoFile = document.getElementById('videoFile').files[0];
-  
-  if (!videoFile) {
-    alert('请先选择一个视频文件');
-    return;
-  }
-  
-  var progressBar = document.getElementById('progress');
-  progressBar.style.width = '0%';
-  
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', '/convert', true);
-  
-  xhr.upload.onprogress = function(event) {
-    if (event.lengthComputable) {
-      var percentComplete = (event.loaded / event.total) * 100;
-      progressBar.style.width = percentComplete + '%';
-    }
-  };
-  
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      var response = JSON.parse(xhr.responseText);
-      var downloadLink = document.getElementById('downloadLink');
-      downloadLink.href = response.imageUrl;
-      downloadLink.download = response.fileName;
-      downloadLink.innerHTML = '点击此处下载序列图';
-      downloadLink.style.display = 'block';
-    }
-  };
-  
-  xhr.send(videoFile);
+function uploadVideo(event) {
+    event.preventDefault();
+    
+    var form = document.getElementById('uploadForm');
+    var formData = new FormData(form);
+    
+    var request = new XMLHttpRequest();
+    request.open('POST', 'convert.php');
+    
+    request.upload.addEventListener('progress', function(event) {
+        var progress = Math.round((event.loaded / event.total) * 100);
+        document.getElementById('progress').innerHTML = '上传进度: ' + progress + '%';
+    });
+    
+    request.addEventListener('load', function(event) {
+        if (request.status == 200) {
+            document.getElementById('result').innerHTML = '转换完成! <a href="' + request.responseText + '">下载序列图</a>';
+        } else {
+            document.getElementById('result').innerHTML = '转换失败!';
+        }
+    });
+    
+    request.send(formData);
 }
